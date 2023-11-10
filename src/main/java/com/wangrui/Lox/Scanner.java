48084e76aab71b78f0cffe58dc3077426547d173
wangrui <wrlyt666@gmail.com>
2023-11-10 10:20:36 +0800

    Scanner

--- src/main/java/com/wangrui/Lox/Scanner.java
+++ src/main/java/com/wangrui/Lox/Scanner.java
@@ -3,7 +3,8 @@ package com.wangrui.Lox;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.wangrui.Lox.TokenType.EOF;
+import static com.wangrui.Lox.TokenType.*;
+
 
 public class Scanner {
     private final String source;
@@ -26,6 +27,61 @@ public class Scanner {
         tokens.add(new Token(EOF,"",null,line));
         return tokens;
     }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            case '!':
+                addToken(match('=')?BANG_EQUAL:BANG);break;
+            case '=':
+                addToken(match('=')?EQUAL_EQUAL:EQUAL);break;
+            case '>':
+                addToken(match('=')?GREATER_EQUAL:GREATER);break;
+            case '<':
+                addToken(match('=')?LESS_EQUAL:LESS);break;
+
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
+        }
+    }
+
+    private char advance(){
+        current++;
+        return source.charAt(current-1);
+    }
+
+    private void addToken(TokenType type){
+        addToken(type,null);
+    }
+    private void addToken(TokenType type, Object literal){
+        String text = source.substring(start,current);
+        tokens.add(new Token(type,text,literal,line));
+    }
+
+    private boolean match(char exepected){
+        if(isAtEnd()) return false;
+        if (source.charAt(current)!=exepected) return false;
+
+        current++;
+        return true;
+    }
+
+
 
 
 }
--- src/main/java/com/wangrui/Lox/Token.java
+++ src/main/java/com/wangrui/Lox/Token.java
@@ -1,12 +1,10 @@
 package com.wangrui.Lox;
 
-import java.util.Objects;
-
 public class Token {
     final TokenType type;  //词素类型
     final String lexeme;  //词素的值
     final Object literal;  //
-    final int line;
+    final int line;  //当前行
 
     public Token(TokenType type, String lexeme, Object literal, int line) {
         this.type = type;
